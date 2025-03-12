7795393c5c4eda8028784eee37ffb051b48fcd8f
zeina101 <zeina.syedain@cas.edu.pk>
2025-03-12 20:30:30 +0500

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,21 @@
+num1=1
+num2=1
+
+print("number one is ", num1)
+print("number two is ", num2)
+print("Addition is ", num1+num2)
+print("Difference is ", num1-num2)
+print("Product is ", num1*num2)
+print("Division is ", num1/num2)
+print("Floor division is ", num1/num2)
+print("Modulus is ", num1%num2)
+print("Square of number one is ", num1**num1)
+print("Square root is ", num1**0.5")
+print("Square of number two is ", num2**num2)
+print("Square root is ", num2**0.5)
+print("Equal? ", num1==num2)
+print("Number One greater? ", num1>num2)
+print("Number two greater? ", num1<num2)
+print("Not equal? ", num1+num2)
+result = num1/2+num2*7+10
+print("Result of equation is ", result)
---
+++ pythonclassproject.py
@@ -0,0 +1,16 @@
+name = "Zeina"
+age = "25"
+is_student = True
+weight = 38.5
+
+print("Name: ", name)
+print("Data Type of Name is", type(name))
+
+print("Age: ", age)
+print("Dat Type of Age is", type(age))
+
+print("is_student: ", is_student)
+print()"Data Type of is_student is ", type(is_student))
+
+print("Weight: ", weight)
+print("Data Type of weight is ", type(weight))
